dfade1e7b5017d05c89fabd93484f1fc6e63a941
Alexiss Diaz <alexiss.diaz@live.com>
2020-01-03 13:03:37 -0600

    added heroku connection settings

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // sets the connection to mysql
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'z12itfj4c1vgopf8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'w1b8x5necgrtroce',
+        password: 'haydj4ontkiqd22c',
+        database: 's567rusj9orfdias'
+    });
+};
 
 
 // this makes the connection
